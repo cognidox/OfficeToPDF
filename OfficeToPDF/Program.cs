d2feab6d6e569e8b644b2d4198e1d294ac04d53d
opensource <opensource@cognidox.com>
2017-03-29 14:20:11 +0100

    [workitem:52] Adds the /word_field_quick_update_safe option
    The /word_field_quick_update_safe option will do a quick field
    update only if no broken links are found

--- OfficeToPDF/Program.cs
+++ OfficeToPDF/Program.cs
@@ -102,6 +102,7 @@ namespace OfficeToPDF
             options["excel_max_rows"] = (int) 0;
             options["excel_worksheet"] = (int) 0;
             options["word_field_quick_update"] = false;
+            options["word_field_quick_update_safe"] = false;
             options["word_no_field_update"] = false;
             options["word_header_dist"] = (float) -1;
             options["word_footer_dist"] = (float) -1;
@@ -124,7 +125,7 @@ namespace OfficeToPDF
             options["pdf_restrict_accessibility_extraction"] = false;
             options["pdf_restrict_full_quality"] = false;
 
-            Regex switches = new Regex(@"^/(version|hidden|markup|readonly|bookmarks|merge|noquit|print|screen|pdfa|template|writepassword|password|help|verbose|exclude(props|tags)|excel_(max_rows|show_formulas|show_headings|auto_macros|active_sheet|worksheet|no_recalculate|no_link_update)|word_(header_dist|footer_dist|ref_fonts|no_field_update|field_quick_update|max_pages|keep_history)|pdf_(page_mode|append|prepend|layout|clean_meta|owner_pass|user_pass|restrict_(annotation|extraction|assembly|forms|modify|print|accessibility_extraction|full_quality))|\?)$", RegexOptions.IgnoreCase);
+            Regex switches = new Regex(@"^/(version|hidden|markup|readonly|bookmarks|merge|noquit|print|screen|pdfa|template|writepassword|password|help|verbose|exclude(props|tags)|excel_(max_rows|show_formulas|show_headings|auto_macros|active_sheet|worksheet|no_recalculate|no_link_update)|word_(header_dist|footer_dist|ref_fonts|no_field_update|field_quick_update(_safe)?|max_pages|keep_history)|pdf_(page_mode|append|prepend|layout|clean_meta|owner_pass|user_pass|restrict_(annotation|extraction|assembly|forms|modify|print|accessibility_extraction|full_quality))|\?)$", RegexOptions.IgnoreCase);
             for (int argIdx = 0; argIdx < args.Length; argIdx++)
             {
                 string item = args[argIdx];
@@ -831,6 +832,7 @@ OfficeToPDF.exe [/bookmarks] [/hidden] [/readonly] input_file [output_file]
   /word_footer_dist <pts>   - The distance (in points) from the footer to the bottom
                               of the page.
   /word_field_quick_update  - Perform a fast update of fields in Word before conversion.
+  /word_field_quick_update_safe - Perform a fast update for fields only if there are no broken linked files.
   /word_keep_history        - Do not clear Word's recent files list.
   /word_max_pages <pages>   - Do not attempt conversion of a Word document if it has more than
                               this number of pages.
