460e432235623d4876c6b16e747449b148a9eb78
Vittal Aithal <vittal.aithal@cognidox.com>
2018-02-12 11:39:48 +0000

    More copyright changes

--- OfficeToPDF/Properties/AssemblyInfo.cs
+++ OfficeToPDF/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cognidox Ltd")]
 [assembly: AssemblyProduct("OfficeToPDF")]
-[assembly: AssemblyCopyright("Copyright © Cognidox Ltd 2017")]
+[assembly: AssemblyCopyright("Copyright © Cognidox Ltd 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.9.0")]
-[assembly: AssemblyFileVersion("1.8.9.0")]
+[assembly: AssemblyVersion("1.8.10.0")]
+[assembly: AssemblyFileVersion("1.8.10.0")]
